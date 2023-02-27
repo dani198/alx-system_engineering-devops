a87f9beaddcdf73a9bd82658d625511732e24d43
dani198 <danyleul16@gmail.com>
2023-02-27 13:34:47 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-#This is a Puppet manifest code that uses the exec resource type to execute 
 exec { 'fix_phpp':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  command => 'strace -e trace=file sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
 }
+
